42616e4021d76f9ac7550f56e4be6f0eea73a9cd
Niall Gaffney <ngaffney@tacc.utexas.edu>
2026-04-25 11:36:39 -0300

    Add files via upload
    First commint of script to run VLLM with hugging face models on TACC systems along with the python requirements.

---
+++ vllm-start.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+MODEL_NAME=$1
+N_GPUS=$2
+N_NODES=$3
+MASTER_IP=$4
+MOE=$5
+rank=$OMPI_COMM_WORLD_RANK
+OTHER_OPTIONS=""
+if [[ $MODEL_NAME == MiniMaxAI* ]] ; then
+#    OTHER_OPTIONS="  --tool-call-parser minimax_m2  --reasoning-parser minimax_m2   --compilation-config '{"mode":3,"pass_config":{"fuse_minimax_qk_norm":true}}'  --enable-auto-tool-choice "
+    OTHER_OPTIONS="  --tool-call-parser minimax_m2  --reasoning-parser minimax_m2 --enable-auto-tool-choice "
+fi
+HEADLESS=""
+if [ "$rank" -ne "0" ]; then
+   HEADLESS="--headless"
+fi
+
+echo vllm serve $1 --tensor-parallel-size $N_GPUS --pipeline-parallel-size $N_NODES --nnodes $N_NODES --node-rank $rank --master-addr $MASTER_IP  --trust-remote-code --distributed-executor-backend "mp" --moe-backend $MOE $OTHER_OPTIONS $HEADLESS
+vllm serve $1 --tensor-parallel-size $N_GPUS --pipeline-parallel-size $N_NODES --nnodes $N_NODES --node-rank $rank --master-addr $MASTER_IP  --trust-remote-code --distributed-executor-backend "mp" --moe-backend $MOE $OTHER_OPTIONS $HEADLESS
+
+
+
